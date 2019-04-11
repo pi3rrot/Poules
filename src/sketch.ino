7938385ae016571d81e77db99271239328d3f61e
pi3rrot <pierre@pi3rrot.net>
2019-04-11 23:07:11 +0200

    first commit

---
+++ src/sketch.ino
@@ -0,0 +1,76 @@
+// Arduino Uno/2009:
+// ----------------------
+// DS3231:  SDA pin   -> Arduino Digital 4 or the dedicated SDA pin
+//          SCL pin   -> Arduino Digital 5 or the dedicated SCL pin
+
+
+#include <Stepper.h>
+#include <DS3231.h>
+
+#include "../lib/calsol.h"
+
+// Pour steppers
+#define STEP_REV 400
+#define REV 10
+
+const int stepsPerRevolution = 400;
+// 360 / 0.9deg
+
+Stepper myStepper1(stepsPerRevolution, 6, 7, 8, 9);
+Stepper myStepper2(stepsPerRevolution, 2, 3, 4, 5);
+
+// Module DS3231 pour l'heure
+DS3231  rtc(4, 5);
+
+struct Date_t {
+        unsigned long annee;
+        unsigned long mois;
+        unsigned long jour;
+};
+
+
+void stepperOff() {
+  for(int i=2; i<=9; i++) {
+    digitalWrite(i, LOW);  
+  }
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  rtc.begin();  
+  // The following lines can be uncommented to set the date and time
+
+  /*  rtc.setDOW(THURSDAY);     // Set Day-of-Week to SUNDAY
+      rtc.setTime(22, 14, 0);     // Set the time to 12:00:00 (24hr format)
+      rtc.setDate(4, 11, 2019);   // Set the date to January 1st, 2014 */
+
+}
+
+void loop() {
+  myStepper2.setSpeed(100);
+  for(int i=0; i<=STEP_REV*REV; i++) {
+      myStepper1.step(1);
+      myStepper2.step(-1);
+  }
+  for(int i=0; i<=STEP_REV*REV; i++) {
+      myStepper1.step(-1);
+      myStepper2.step(1);
+  }
+  stepperOff();
+  delay(3000);
+
+ // Send Day-of-Week
+  Serial.print(rtc.getDOWStr());
+  Serial.print(" ");
+  
+  // Send date
+  Serial.print(rtc.getDateStr());
+  Serial.print(" -- ");
+
+  // Send time
+  Serial.println(rtc.getTimeStr());
+  
+  // Wait one second before repeating :)
+  delay (1000);
+}
